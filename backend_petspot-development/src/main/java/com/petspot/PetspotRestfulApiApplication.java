c8f2cdada9035449238e897bd42488408dcb6994
Rique-Borges <henriqueribeiroborges@gmail.com>
2024-05-30 18:17:19 -0300

    first commit

---
+++ backend_petspot-development/src/main/java/com/petspot/PetspotRestfulApiApplication.java
@@ -0,0 +1,12 @@
+package com.petspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetspotRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetspotRestfulApiApplication.class, args);
+    }
+}
